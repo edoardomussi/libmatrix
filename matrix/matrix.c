14649b291289377053d9fb2248712cd01b263fda
edoardomussi <mussi.edoardo@hotmail.it>
2026-06-15 22:09:51 +0200

    Fixed pointer behaviour, aligned the header file.

--- matrix/matrix.c
+++ matrix/matrix.c
@@ -3,74 +3,122 @@
 #include <stdbool.h>
 #include "matrix.h"
 
-matrix_t* mnew(int r, int c) {
-    matrix_t* m = malloc(sizeof(matrix_t));
-    m->rows = r;
-    m->columns = c;
-    m->data = calloc(r*c, sizeof(float));
-    return m;
+int mnew(int rows, int cols, matrix_t** m_out) {
+    // Allocate the outer structure
+    *m_out = malloc(sizeof(matrix_t));
+    if ((*m_out) == NULL) return MAT_ERROR_ALLOCATION_FAILED;
+
+    // Initialize the inner values
+    (*m_out)->rows = rows;
+    (*m_out)->columns = cols;
+    // Allocate the inner structure
+    (*m_out)->data = calloc(rows*cols, sizeof(float));
+    if ((*m_out)->data == NULL) {
+        free(*m_out);
+        *m_out = NULL;
+        return MAT_ERROR_ALLOCATION_FAILED;
+    };
+
+    return MAT_SUCCESS;
 };
 
-void mfree(matrix_t* m) {
-    free(m->data);
-    free(m);
+int mfree(matrix_t** m) {
+    if (*m == NULL || (*m)->data == NULL) return MAT_ERROR_NULL_POINTER;
+    
+    free((*m)->data);
+    free(*m);
+
+    return MAT_SUCCESS;
 };
 
-void mprint(matrix_t* m) {
+int mprint(const matrix_t* m) {
+    if (m == NULL) return MAT_ERROR_NULL_POINTER;
+
     for (int i=0; i < m->rows; i++) {
         for (int j=0; j < m->columns; j++) {
-            printf("%f", *(m->data+(i*m->columns)+j));
-        }
+            printf("%f ", *(m->data+(i*m->columns)+j));
+        };
         printf("\n");
-    }
-    return;
+    };
+    printf("\n");
+
+    return MAT_SUCCESS;
 };
 
-int mwrite(matrix_t* m, int row, int col, float val) {
-    if ((col-1) > m->columns || (row-1) > m->rows) return 1;
-    *(m->data+((row-1)*m->columns)+col-1) = val;
-    return 0;
+int mwrite(matrix_t** m, int row, int col, float val) {
+    if (*m == NULL) return MAT_ERROR_NULL_POINTER;
+    if (col < 0 || row <0 || col >= (*m)->columns || row >= (*m)->rows) return MAT_ERROR_OUT_OF_BOUNDS;
+
+    *((*m)->data+(row*(*m)->columns)+col) = val;
+
+    return MAT_SUCCESS;
 };
 
-float mread(matrix_t* m, int row, int col) {
-    if ((col-1) > m->columns || (row-1) > m->rows) return (float)1;
-    return *(m->data+((row-1)*m->columns)+col-1);
+int mread(const matrix_t* m, int row, int col, float* res) {
+    if (m == NULL) return MAT_ERROR_NULL_POINTER;
+    if (col < 0 || row < 0 || col > m->columns || row > m->rows) return MAT_ERROR_OUT_OF_BOUNDS;
+
+    *res = *(m->data+(row*m->columns)+col);
+
+    return MAT_SUCCESS;
 };
 
-matrix_t* madd(matrix_t* a, matrix_t* b) {
-    if (a->columns != b->columns || a->rows != b->rows) return NULL;
+int mtpose(const matrix_t* m, matrix_t** m_out) {
+    if (m == NULL || (*m_out) == NULL) return MAT_ERROR_NULL_POINTER;
+    if (m->rows != (*m_out)->columns || m->columns != (*m_out)->rows) return MAT_ERROR_DIMENSION_MISMATCH;
 
-    matrix_t* m = mnew(a->rows, b->columns);
+    for (int i=0; i<m->rows; i++) {
+        for (int j=0; j<m->columns; j++) {
+            *((*m_out)->data+(j*(*m_out)->columns)+i) = *(m->data+(i*m->columns)+j);
+        };
+    };
+
+    return MAT_SUCCESS;
+};
+
+int madd(const matrix_t* a, const matrix_t* b, matrix_t** m_out) {
+    if (a == NULL || b == NULL || *m_out == NULL) return MAT_ERROR_NULL_POINTER;
+    if (a->columns != b->columns ||
+        a->rows != b->rows ||
+        (*m_out)->columns != a->columns ||
+        (*m_out)->rows != a->rows)
+        return MAT_ERROR_DIMENSION_MISMATCH;
 
     for (int i=0; i < a->rows; i++) {
         for (int j=0; j < a->columns; j++) {
-            *(m->data+(i*m->columns)+j) = *(a->data+(i*a->columns)+j) + (*(b->data+(i*b->columns)+j));
-        }
-    }
-    return m;
+            *((*m_out)->data+(i*(*m_out)->columns)+j) = *(a->data+(i*a->columns)+j) + (*(b->data+(i*b->columns)+j));
+        };
+    };
+
+    return MAT_SUCCESS;
 };
 
-void mmul_scalar(matrix_t* m, float s) {
+int mmul_scalar(const matrix_t* m, float s, matrix_t** m_out) {
+    if (m == NULL || *m_out == NULL) return MAT_ERROR_NULL_POINTER;
+
     for (int i=0; i < m->rows; i++) {
         for (int j=0; j < m->columns; j++) {
-            *(m->data+(i*m->columns)+j) *= s;
-        }
-    }
-    return;
+            *((*m_out)->data+(i*(*m_out)->columns)+j) = *(m->data+(i*m->columns)+j)*s;
+        };
+    };
+
+    return MAT_SUCCESS;
 };
 
-matrix_t* mmul(matrix_t* a, matrix_t* b) {
-    if (a->columns != b->rows) return NULL;
-
-    matrix_t* m = mnew(a->rows, b->columns);
+int mmul(const matrix_t* a, const matrix_t* b, matrix_t** m_out) {
+    if (a == NULL || b == NULL || *m_out == NULL) return MAT_ERROR_NULL_POINTER;
+    if (a->columns != b->rows ||
+        (*m_out)->rows != a ->rows ||
+        (*m_out)->columns != b-> columns)
+        return MAT_ERROR_DIMENSION_MISMATCH;
 
     for(int i=0; i < a->rows; i++) {
         for(int j=0; j < b->columns; j++) {
             for (int k=0; k < a->columns; k++) {
-                *(m->data+(i*m->columns)+j) += *(a->data+(i*a->columns)+k) * (*(b->data+(k*b->columns)+j)) ;
+                *((*m_out)->data+(i*(*m_out)->columns)+j) += *(a->data+(i*a->columns)+k) * (*(b->data+(k*b->columns)+j)) ;
             }
         }
     }
-    return m;
+    return MAT_SUCCESS;
 }
 
--- matrix/matrix.h
+++ matrix/matrix.h
@@ -2,7 +2,7 @@
 #define MATRIX_H
 
 /**
- * \brief A structure representing a 2D matrix stored as a flat array.
+ * @brief A structure representing a 2D matrix stored as a flat array.
  */
 typedef struct {
     int rows;
@@ -11,65 +11,93 @@ typedef struct {
 } matrix_t;
 
 /**
- *  \brief Allocate a new matrix initialized with zeroes.
- *  \param r Number of rows.
- *  \param c Number of columns.
- *  \return Pointer to the newly allocated matrix.
+ * @brief Enumeration of all handled status codes.
  */
-matrix_t* mnew(int rows, int cols);
+typedef enum {
+    MAT_SUCCESS = 0,
+    MAT_ERROR_NULL_POINTER = 1,
+    MAT_ERROR_OUT_OF_BOUNDS = 2,
+    MAT_ERROR_DIMENSION_MISMATCH = 3,
+    MAT_ERROR_NOT_SQUARE = 4,
+    MAT_ERROR_ALLOCATION_FAILED = 5
+} matrix_status_t;
 
 /**
- * \brief De-allocate a matrix.
- * \param m Matrix to be freed.
- * \return Your memory, hopefully.
+ *  @brief Allocate a new matrix initialized with zeroes.
+ *  @param rows Number of rows.
+ *  @param cols Number of columns.
+ *  @param m Pointer to the new matrix.
+ *  @return matrix_status_t Status code of the operation.
  */
-void mfree(matrix_t* m);
+int mnew(int rows, int cols, matrix_t** m_out);
 
 /**
- * \brief Print a matrix on stdout.
- * \param m Matrix to be printed.
+ * @brief De-allocate a matrix.
+ * @param m Matrix to be freed.
+ * @return matrix_status_t Status code of the operation.
  */
-void mprint(matrix_t* m);
+int mfree(matrix_t** m);
 
 /**
- * \brief Write a single `float` value to a position in a matrix.
- * \param m Matrix to write to.
- * \param row Row number to write to.
- * \param col Column number to write to.
- * \param val `float` value to write in the matrix.
+ * @brief Print a matrix on stdout.
+ * @param m Matrix to be printed.
+ *  @return matrix_status_t Status code of the operation.
  */
-int mwrite(matrix_t* m, int row, int col, float val);
+int mprint(const matrix_t* m);
 
 /**
- * \brief Read a value from a matrix.
- * \param m Matrix to read from.
- * \param row Row number to read from.
- * \param col Column number to read from.
+ * @brief Write a single `float` value to a position in a matrix.
+ * @param m Matrix to write to.
+ * @param row Row number to write to.
+ * @param col Column number to write to.
+ * @param val `float` value to write in the matrix.
+ * @return matrix_status_t Status code of the operation.
  */
-float mread(matrix_t* m, int row, int col);
+int mwrite(matrix_t** m, int row, int col, float val);
 
 /**
- * \brief Add two matrices together in a new matrix.
- * \param a First matrix to be addedd.
- * \param b Second matrix to be addedd.
- * \return matrix_t* Pointer to the resulting matrix.
+ * @brief Read a value from a matrix.
+ * @param m Matrix to read from.
+ * @param row Row number to read from.
+ * @param col Column number to read from.
+ * @param res Pointer to the variable to store the result in.
+ * @return matrix_status_t Status code of the operation.
  */
-matrix_t* madd(matrix_t* a, matrix_t* b);
+int mread(const matrix_t* m, int row, int col, float* res);
 
 /**
- * \brief Multiplicate a matrix by a scalar value.
- * \param m Matrix to be multiplied.
- * \param s Scalar value.
- * \return Nothing, the original matrix is overwritten.
+ * @brief Transposes a matrix.
+ * @param m Matrix to be transposed.
+ * @param m_out Pointer to the transposed matrix.
+ * @return matrix_status_t Status code of the operation.
  */
-void mmul_scalar(matrix_t* m, float s);
+int mtpose(const matrix_t* m, matrix_t** m_out);
 
 /**
- * \brief Multiplicate two matrices.
- * \param a First matrix to be multiplied.
- * \param b Second matrix to be multiplied.
- * \return matrix_t* Pointer to the resulting matrix.
+ * @brief Add two matrices together in a new matrix.
+ * @param a First matrix to be addedd.
+ * @param b Second matrix to be addedd.
+ * @param m_out Matrix pointer to the resulting matrix.
+ * @return matrix_status_t Status code of the operation.
  */
-matrix_t* mmul(matrix_t* a, matrix_t* b);
+int madd(const matrix_t* a, const matrix_t* b, matrix_t** m_out);
+
+/**
+ * @brief Multiplicate a matrix by a scalar value.
+ * @param m Matrix to be multiplied.
+ * @param s Scalar value.
+ * @param m_out Matrix pointer to the resulting matrix.
+ * @return matrix_status_t Status code of the operation.
+ */
+int mmul_scalar(const matrix_t* m, float s, matrix_t** m_out);
+
+/**
+ * @brief Multiplicate two matrices.
+ * @param a First matrix to be multiplied.
+ * @param b Second matrix to be multiplied.
+ * @param m_out Matrix pointer to the resulting matrix.
+ * @return matrix_status_t Status code of the operation.
+ */
+int mmul(const matrix_t* a, const matrix_t* b, matrix_t** m_out);
 
 #endif
